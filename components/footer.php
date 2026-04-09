5fccfc8ee38b8ececb32725ac8343a5f0bf71438
widhyasee <faridasee8@gmail.com>
2026-04-09 21:23:01 +0700

    revisi 09/06/26

---
+++ components/footer.php
@@ -0,0 +1,37 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>Our Email</h3>
+         <a href="mailto:bangresto@gmail.com">bangresto@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>Opening Hours</h3>
+         <p>11:00 - 23:00</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>Our Address</h3>
+         <a href="#">Alun-Alun, Depok - 400104</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>Our Number</h3>
+         <a href="tel:1234567890">123-456-7890</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Kelompok Be Better</span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/new_loader.gif" alt="">
+</div>
